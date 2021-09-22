60cb7a1924d55eebf81b050142cdcef2a2ceedc2
Nicolas Andres Calvo <nico.calvo2014@gmail.com>
2021-09-21 21:12:07 -0300

    add register and login

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(morgan('tiny'));
 
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
 
+app.get('/login', (req, res) =>{res.sendFile(path.resolve('./views/login.html'))});
+
+app.get('/register', (req, res) =>{res.sendFile(path.resolve('./views/register.html'))});
+
 app.listen(process.env.PORT || PORT, () => console.log(`Example app listening on port port!`));
